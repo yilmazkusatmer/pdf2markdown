c2ba4e67576ba38f9c37c48980b82d0c83c154dc
Jorben <jorbenzhu@gmail.com>
2025-03-20 00:11:35 +0800

    chore: Modify version to 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="markpdfdown",
-    version="0.1.0",
+    version="1.0.4",
     packages=find_packages(),
     install_requires=[
         # 从 requirements.txt 中读取依赖
